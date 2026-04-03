c667e3ee19e5fd7f991d5445ed3111388c4e5e90
ulayaw-rex <jhonrextabanas27@gmail.com>
2026-04-03 20:32:05 +0800

    cors-origins

--- server/config/cors.php
+++ server/config/cors.php
@@ -24,6 +24,8 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://hse-archive.vercel.app',
+    'https://hse-archive-8dtlt1ewr-ulayaw-rexs-projects.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
