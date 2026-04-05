a95447b39f235db7ff41637639687447004c908f
ulayaw-rex <jhonrextabanas27@gmail.com>
2026-04-05 16:13:29 +0800

    define brevo

--- server/config/mail.php
+++ server/config/mail.php
@@ -49,6 +49,11 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
+        'brevo' => [
+            'transport' => 'symfony',
+            'dsn' => 'brevo+api://' . env('BREVO_KEY') . '@default',
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
